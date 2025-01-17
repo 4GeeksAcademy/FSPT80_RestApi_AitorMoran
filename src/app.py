18b8d107f6866083a09de988f20c92ba2494d505
AitorMoran <aitorfm8@gmail.com>
2025-01-17 18:25:42 +0000

    First Commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, People, Favorites, Planets, Planetforites
 #from models import Person
 
 app = Flask(__name__)
@@ -32,6 +32,174 @@ def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+#obtener people________________________
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    data = People.query.all() 
+    people_list = [el.serialize() for el in data] 
+
+    return jsonify(people_list), 200
+
+#obtener people id________________________
+
+@app.route('/people/<int:id>', methods=['GET'])
+def get_one(id):
+    data = People.query.get(id) 
+    if not data: return jsonify({"error": "Persona no encontrada"}), 404
+    
+    return jsonify(data.serialize()), 200
+
+#añadir people________________________
+
+@app.route('/newpeople', methods=['POST'])
+def new_people():
+    try:
+        data = request.json
+        
+        newPeople = People(
+            id = data['id'],
+            name = data['name'],
+            lastname = data['lastname'],
+            side = data['side']
+        )
+        db.session.add(newPeople)
+        db.session.commit()
+        return jsonify("Personaje Creado"), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)})
+
+#eliminar people________________________
+
+@app.route('/deletepeople/<int:id>', methods=['DELETE'])
+def delete_people(id):
+    try:
+        data = People.query.get(id)
+        if data is None:
+            raise Exception("No se encontró el sujeto")
+        
+        db.session.delete(data)
+        db.session.commit()
+        return jsonify({"message": "Sujeto eliminado"}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 400
+
+
+#añadir people a favoritos________________________
+
+@app.route('/favorites/<int:person_id>', methods=['POST'])
+def add_favorite(person_id):
+    try:
+        data = request.json
+        
+        person = People.query.get(person_id)
+        if not person:
+            raise Exception("Persona no encontrada")
+        
+        new_favorite = Favorites(
+            person_id = person_id
+        )
+        db.session.add(new_favorite)
+        db.session.commit()
+
+        return jsonify("Sujeto añadido a favoritos"), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 400
+    
+
+#obtener planets________________________
+
+@app.route('/planets', methods=['GET'])
+def get_planet():
+    data = Planets.query.all() 
+    planet_list = [el.serialize() for el in data] 
+
+    return jsonify(planet_list), 200
+
+#obtener planets id________________________
+
+@app.route('/planets/<int:id>', methods=['GET'])
+def get_ones(id):
+    data = Planets.query.get(id) 
+    if not data: return jsonify({"error": "Planeta no encontrada"}), 404
+    
+    return jsonify(data.serialize()), 200
+
+#añadir planets________________________
+
+@app.route('/newplanet', methods=['POST'])
+def new_planet():
+    try:
+        data = request.json
+        
+        new_planet = Planets(
+            id = data['id'],
+            name = data['name'],
+            terrain = data['terrain'],
+            population = data['population'],
+            galaxy = data['galaxy']
+        )
+        db.session.add(new_planet)
+        db.session.commit()
+        return jsonify("Planeta Creado"), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)})
+
+#eliminar planets________________________
+
+@app.route('/deleteplanet/<int:id>', methods=['DELETE'])
+def delete_planet(id):
+    try:
+        data = Planets.query.get(id)
+        if data is None:
+            raise Exception("No se encontró el planeta")
+        
+        db.session.delete(data)
+        db.session.commit()
+        return jsonify({"message": "Planeta eliminado"}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 400
+
+
+#añadir planets a favoritos________________________
+
+@app.route('/planetforites/<int:planet_id>', methods=['POST'])
+def add_planetforites(planet_id):
+    try:
+        data = request.json
+        
+        planet = Planets.query.get(planet_id)
+        if not planet:
+            raise Exception("Planeta no encontrado")
+        
+        new_favorite = Planetforites(
+            planet_id = planet_id
+        )
+        db.session.add(new_favorite)
+        db.session.commit()
+
+        return jsonify("Planeta añadido a favoritos"), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 400
+
+
+
+
+
+
+
+
+
+
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
--- src/models.py
+++ src/models.py
@@ -2,18 +2,81 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+#TABLA PEOPLE ______________________________________________________
+
+class People (db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(150), nullable=False)
+    lastname = db.Column(db.String(150), nullable=False)
+    side = db.Column(db.String(150), nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<People %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "lastname": self.lastname,
+            "side": self.side
+        }
+    
+
+#TABLA PLANETAS ______________________________________________________
+
+class Planets (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    terrain = db.Column(db.String(150), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    galaxy = db.Column(db.String(150), nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "terrain": self.terrain,
+            "population": self.population,
+            "galaxy": self.galaxy
+        }
+    
+
+#TABLA FAVORITOS PEOPLE ______________________________________________________
+
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=False)
+
+    person = db.relationship('People', backref=db.backref('favorites', lazy=True))
+
+    def __repr__(self):
+        return f'<Favorites {self.person_id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "person_id": self.person_id
+        }
+    
+
+#TABLA FAVORITOS PLANETS ______________________________________________________
+
+
+class Planetforites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=False)
+
+    planet = db.relationship('Planets', backref=db.backref('planetforites', lazy=True))
+
+    def __repr__(self):
+        return f'<Planetforites {self.planet_id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet_id": self.planet_id
         }
